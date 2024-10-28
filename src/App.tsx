6e84721f94ebd55c945fcdd30f9e01e0e916c359
Mikkel <mikkelelvers@outlook.com>
2024-10-28 23:56:28 +0100

    added logo to navbar

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import Hero from "./components/Hero";
 import Footer from "./components/Footer";
 import Gradient from "./components/Gradient";
 import Projects from "./components/Projects";
+import Navbar from "./components/Navbar";
 
 export default function App() {
   return (
     <div className="relative h-full overflow-y-auto antialiased bg-black">
+      <Navbar />
       <Gradient />
       <div className="fixed inset-0 bg-fixed bg-cover bg-center"></div>
       <div className="relative z-10 flex flex-col items-center p-4 space-y-8 container mx-auto">
