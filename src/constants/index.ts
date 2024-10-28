77f72807bb9111d14ad05b616f5ba5359024beb7
Mikkel <mikkelelvers@outlook.com>
2024-10-28 23:48:25 +0100

    Added project section

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -7,3 +7,48 @@ export const about = [
     "architecto rem beatae maxime eius, facilis velit. Cumque ",
     "quod suscipit!"
 ]
+
+export const projects = [
+    {
+        id: 1,
+        name: "Placeholder Project 1",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, reiciendis blanditiis eligendi, labore iure magni architecto rem beatae maxime eius, facilis velit. Cumque quod suscipit!",
+        image: "https://placehold.co/350x350",
+        link: ""
+    },
+    {
+        id: 2,
+        name: "Placeholder Project 2",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, reiciendis blanditiis eligendi, labore iure magni architecto rem beatae maxime eius, facilis velit. Cumque quod suscipit!",
+        image: "https://placehold.co/350x350",
+        link: ""
+    },
+    {
+        id: 3,
+        name: "Placeholder Project 3",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, reiciendis blanditiis eligendi, labore iure magni architecto rem beatae maxime eius, facilis velit. Cumque quod suscipit!",
+        image: "https://placehold.co/350x350",
+        link: ""
+    },
+    {
+        id: 4,
+        name: "Placeholder Project 4",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, reiciendis blanditiis eligendi, labore iure magni architecto rem beatae maxime eius, facilis velit. Cumque quod suscipit!",
+        image: "https://placehold.co/350x350",
+        link: ""
+    },
+    {
+        id: 5,
+        name: "Placeholder Project 5",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, reiciendis blanditiis eligendi, labore iure magni architecto rem beatae maxime eius, facilis velit. Cumque quod suscipit!",
+        image: "https://placehold.co/350x350",
+        link: ""
+    },
+    {
+        id: 6,
+        name: "Placeholder Project 6",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, reiciendis blanditiis eligendi, labore iure magni architecto rem beatae maxime eius, facilis velit. Cumque quod suscipit!",
+        image: "https://placehold.co/350x350",
+        link: ""
+    },
+]
