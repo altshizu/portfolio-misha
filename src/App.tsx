044c91c6ca011a04bd5259205e8aba19c0791a3e
mikkel <mikkelelvers@outlook.com>
2024-10-30 08:56:59 +0100

    added about section

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Hero from "./components/Hero";
 import Footer from "./components/Footer";
 import Gradient from "./components/Gradient";
 import Projects from "./components/Projects";
+import About from "./components/About";
 import Navbar from "./components/Navbar";
 
 export default function App() {
@@ -12,6 +13,9 @@ export default function App() {
       <div className="fixed inset-0 bg-fixed bg-cover bg-center"></div>
       <div className="relative z-10 flex flex-col items-center p-4 space-y-8 container mx-auto">
         <Hero />
+        <hr className="w-full border-t border-white opacity-30" />
+        <div className="mb-1"></div>
+        <About />
         <hr className="w-full border-t border-white opacity-30" />
         <div className="mb-1"></div>
         <Projects />
---
+++ src/components/About.tsx
@@ -0,0 +1,27 @@
+export default function About() {
+    return (
+        <section className="pb-[5rem] text-white">
+            <h2 className="mb-10 text-center text-4xl lg:text-6xl">About</h2>
+            <h3 className="text-3xl text-left">
+                Header 1
+            </h3>
+            <p className="mb-5">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam delectus temporibus officiis? Ut vitae possimus illum, eveniet praesentium voluptatem maxime aperiam exercitationem aliquid ad quam saepe reiciendis dicta nesciunt assumenda, vero earum mollitia magnam corrupti! Hic laboriosam perferendis recusandae consequatur qui laudantium distinctio quia nostrum est deleniti, neque, porro corrupti.
+            </p>
+
+            <h3 className="text-3xl text-right">
+                Header 2
+            </h3>
+            <p className="mb-5">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam delectus temporibus officiis? Ut vitae possimus illum, eveniet praesentium voluptatem maxime aperiam exercitationem aliquid ad quam saepe reiciendis dicta nesciunt assumenda, vero earum mollitia magnam corrupti! Hic laboriosam perferendis recusandae consequatur qui laudantium distinctio quia nostrum est deleniti, neque, porro corrupti.
+            </p>
+
+            <h3 className="text-3xl text-left">
+                Header 3
+            </h3>
+            <p className="mb-5">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam delectus temporibus officiis? Ut vitae possimus illum, eveniet praesentium voluptatem maxime aperiam exercitationem aliquid ad quam saepe reiciendis dicta nesciunt assumenda, vero earum mollitia magnam corrupti! Hic laboriosam perferendis recusandae consequatur qui laudantium distinctio quia nostrum est deleniti, neque, porro corrupti.
+            </p>
+        </section>
+    )
+}
